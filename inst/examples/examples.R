9edd4613d407df5a221fa433dfe429e289739aa4
timelyportfolio <kent.russell@timelyportfolio.com>
2015-10-26 10:57:53 -0500

    add tmap itmap example to see how it works with svg

--- inst/examples/examples.R
+++ inst/examples/examples.R
@@ -21,3 +21,28 @@ browsable(
     tooltipster()
   )
 )
+
+library(tmap)
+library(dplyr)
+data(World)
+data(metro)
+metro$growth <- (metro$pop2020 - metro$pop2010) / (metro$pop2010 * 10) * 100
+
+(tm_shape(World) +
+  tm_polygons("income_grp", palette="-Blues", contrast=.7, id="name", title="Income group") +
+  tm_shape(metro) +
+  tm_bubbles("pop2010", col = "growth", 
+             border.col = "black", border.alpha = .5, 
+             style="fixed", breaks=c(-Inf, seq(0, 6, by=2), Inf),
+             palette="-RdYlBu", contrast=1, 
+             title.size="Metro population", 
+             title.col="Growth rate (%)", id="name") + 
+  tm_layout(legend.bg.color = "grey90", legend.bg.alpha=.5, legend.frame=TRUE, asp=0)) %>% 
+  itmap() -> itm
+
+browsable(
+  tagList(
+    itm,
+    tooltipster("g")
+  )
+)
