c1aa6c9f8d9775693d0efff8ca449eb1cf75632b
timelyportfolio <kent.russell@timelyportfolio.com>
2015-11-05 11:39:00 -0600

    remove shiny and add documentation

--- R/tooltipster.R
+++ R/tooltipster.R
@@ -1,6 +1,12 @@
-#' <Add Title>
+#' Tooltips htmlwidget
 #'
-#' <Add Description>
+#' Add tooltips to HTML content using the tooltipster jQuery library.
+#' 
+#' @param selector any valid \code{CSS} selector.  By default,
+#'           \code{".tooltip"} will be provided.
+#' @param ... \code{list} of options for \code{tooltipster}.  See
+#'           \href{https://iamceege.github.io/tooltipster}{toolipster options}
+#'           for a complete reference.
 #'
 #' @import htmlwidgets
 #'
@@ -30,32 +36,4 @@ tooltipster_html <- function(id, style, class, ...){
     style = "display:none;",
     class = class
   )
-}
-
-#' Shiny bindings for tooltipster
-#'
-#' Output and render functions for using tooltipster within Shiny
-#' applications and interactive Rmd documents.
-#'
-#' @param outputId output variable to read from
-#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
-#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
-#'   string and have \code{'px'} appended.
-#' @param expr An expression that generates a tooltipster
-#' @param env The environment in which to evaluate \code{expr}.
-#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
-#'   is useful if you want to save an expression in a variable.
-#'
-#' @name tooltipster-shiny
-#'
-#' @export
-tooltipsterOutput <- function(outputId, width = '100%', height = '400px'){
-  shinyWidgetOutput(outputId, 'tooltipster', width, height, package = 'tooltipsterR')
-}
-
-#' @rdname tooltipster-shiny
-#' @export
-renderTooltipster <- function(expr, env = parent.frame(), quoted = FALSE) {
-  if (!quoted) { expr <- substitute(expr) } # force quoted
-  shinyRenderWidget(expr, tooltipsterOutput, env, quoted = TRUE)
 }
